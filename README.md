4256c52d4d0073af2b40988ce1cadcc1202c9ef1
Pau Bofill <pcb2002ma@gmail.com>
2025-01-08 10:08:35 +0100

    initial commit tutorial: https://github.com/disseny-de-software/tutorial_acs_md/blob/main/tutorial_acs_md.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# fita3
+
+- A Flutter Project, connected to a [java backend](https://github.com/Paub2002/DS)
+
+- This project stardted following: [tutorial](https://github.com/disseny-de-software/tutorial_acs_md/blob/main/tutorial_acs_md.md)
+
---
+++ lib/screen_space.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'tree.dart';
+import 'requests.dart';
+class ScreenSpace extends StatefulWidget {
+  final String id;
+  const ScreenSpace({super.key, required this.id});
+
+  @override
+  State<ScreenSpace> createState() => _ScreenSpaceState();
+}
+
+class _ScreenSpaceState extends State<ScreenSpace> {
+  late Future<Tree> futureTree;
+
+  @override
+  void initState() {
+    super.initState();
+    futureTree = getTree(widget.id);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Tree>(
+      future: futureTree,
+      builder: (context, snapshot) {
+        // anonymous function
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              foregroundColor: Theme.of(context).colorScheme.onPrimary,
+              title: Text(snapshot.data!.root.id),
+              actions: <Widget>[
+                IconButton(icon: const Icon(Icons.home), onPressed: () {}
+                  // TODO go home page = root
+                ),
+                //TODO other actions
+              ],
+            ),
+            body: ListView.separated(
+              // it's like ListView.builder() but better because it includes a separator between items
+              padding: const EdgeInsets.all(16.0),
+              itemCount: snapshot.data!.root.children.length,
+              itemBuilder: (BuildContext context, int i) =>
+                  _buildRow(snapshot.data!.root.children[i], i),
+              separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        // By default, show a progress indicator
+        return Container(
+            height: MediaQuery.of(context).size.height,
+            color: Colors.white,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ));
+      },
+    );
+  }
+  Widget _buildRow(Door door, int index) {
+    return ListTile(
+      title: Text('D ${door.id}'),
+      trailing: Text('${door.state}, closed=${door.closed}'),
+    );
+  }
+}
+
